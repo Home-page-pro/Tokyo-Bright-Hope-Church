cab5c4421047177972ae2f0ec4aa0762f8650f42
toshihito <soccerdancelike@yahoo.ne.jp>
2020-05-31 21:54:40 +0900

    12

--- js/main.js
+++ js/main.js
@@ -4,8 +4,20 @@ $(function () {
   }, 500); //0.5秒後にロゴをフェードイン!
   setTimeout(function () {
     $('.start').fadeOut(500);
-  }, 3500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+  }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
+
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+});
+
 
 function main() {
 
