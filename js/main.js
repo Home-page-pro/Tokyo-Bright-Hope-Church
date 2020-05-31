c99eacc9e1d477a097c4ba44c246cfeaa24c9819
toshihito <soccerdancelike@yahoo.ne.jp>
2020-05-31 23:11:44 +0900

    14

--- js/main.js
+++ js/main.js
@@ -7,17 +7,6 @@ $(function () {
   }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
 
-$(function () {
-  $('a[href^="#"]').click(function () {
-    var speed = 500;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
-});
-
 
 function main() {
 
